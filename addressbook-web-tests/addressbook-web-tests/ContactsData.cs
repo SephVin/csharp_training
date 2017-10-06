b95be9f69609bb15106a8c129625c384a15faae3
SephVin <vinseph236@gmail.com>
2017-10-07 00:40:17 +0500

    Задание №4

---
+++ addressbook-web-tests/addressbook-web-tests/ContactsData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactsData
+    {
+        private string firstName;
+        private string lastName;
+
+        public ContactsData(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+    }
+}
